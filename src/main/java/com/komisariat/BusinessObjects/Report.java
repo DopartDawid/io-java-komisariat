6e0e8f85f3160998997498458420734df3760ba4
Maciej Franikowski <mfranikowski@gmail.com>
2020-12-07 16:25:04 +0100

    Dodane pliki do xml do Report, Shift, Street, Tool. Napisano odpowiednie klasy

--- src/main/java/com/komisariat/BusinessObjects/Report.java
+++ src/main/java/com/komisariat/BusinessObjects/Report.java
@@ -1,9 +1,43 @@
 package com.komisariat.BusinessObjects;
 
+import java.util.Date;
+
 public class Report {
 
+	private int id;
 	private String title;
-	private int date;
+	private Date date;
 	private String content;
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
 }
--- src/main/java/com/komisariat/BusinessObjects/Shift.java
+++ src/main/java/com/komisariat/BusinessObjects/Shift.java
@@ -1,13 +1,79 @@
 package com.komisariat.BusinessObjects;
 
+import java.util.Date;
+
 public class Shift {
 
-	private int startDate;
-	private int endDate;
-	private int patrolRegion;
+	private int id;
+	private Date startDate;
+	private Date endDate;
+	private PatrolRegion patrolRegion;
 	private Report report;
 	private Kit kit;
 	private Vehicle vehicle;
 	private Officer officer;
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public PatrolRegion getPatrolRegion() {
+		return patrolRegion;
+	}
+
+	public void setPatrolRegion(PatrolRegion patrolRegion) {
+		this.patrolRegion = patrolRegion;
+	}
+
+	public Report getReport() {
+		return report;
+	}
+
+	public void setReport(Report report) {
+		this.report = report;
+	}
+
+	public Kit getKit() {
+		return kit;
+	}
+
+	public void setKit(Kit kit) {
+		this.kit = kit;
+	}
+
+	public Vehicle getVehicle() {
+		return vehicle;
+	}
+
+	public void setVehicle(Vehicle vehicle) {
+		this.vehicle = vehicle;
+	}
+
+	public Officer getOfficer() {
+		return officer;
+	}
+
+	public void setOfficer(Officer officer) {
+		this.officer = officer;
+	}
 }
