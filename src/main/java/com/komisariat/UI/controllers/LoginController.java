de277504c45be652dcb63704e04cdb6dba48730a
Dawid Dopart <dawid.dopart@gmail.com>
2021-01-21 19:05:05 +0100

    password is PassField instead of TextField

--- src/main/java/com/komisariat/UI/controllers/LoginController.java
+++ src/main/java/com/komisariat/UI/controllers/LoginController.java
@@ -3,6 +3,7 @@ package com.komisariat.UI.controllers;
 import com.komisariat.UI.GraphicalUI;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 import javax.swing.*;
@@ -13,7 +14,7 @@ public class LoginController {
     public TextField l_input;
 
     @FXML
-    public TextField p_input;
+    public PasswordField p_input;
 
     private GraphicalUI ui = GraphicalUI.getInstance();
     private static LoginController instance;
